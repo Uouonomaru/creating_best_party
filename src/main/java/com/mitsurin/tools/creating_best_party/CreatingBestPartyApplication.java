039515d57fdbbec34d215d96687b544c8ba9fbf0
Uouonomaru <coffeeyuito1227@gmail.com>
2021-07-27 02:42:03 +0900

    Initialized project repostiory

---
+++ src/main/java/com/mitsurin/tools/creating_best_party/CreatingBestPartyApplication.java
@@ -0,0 +1,13 @@
+package com.mitsurin.tools.creating_best_party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingBestPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingBestPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitsurin/tools/creating_best_party/CreatingBestPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitsurin.tools.creating_best_party;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingBestPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
